16c19a1577bad31e1207f087e79b35982c512800
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-01-31 12:17:23 +0000

    Started setting up classes for different employees

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// import Employee from ("./lib/Employee.js");
+const Employee = require("./lib/Employee.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
